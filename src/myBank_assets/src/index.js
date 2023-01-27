b65761bca9b3d109b334612c2aa682c37d456cdc
OkolieOKS234 <dokolie040@gmail.com>
2023-01-27 13:09:26 +0100

    Initial commit.

---
+++ src/myBank_assets/src/index.js
@@ -0,0 +1,19 @@
+import { myBank } from "../../declarations/myBank";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await myBank.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
