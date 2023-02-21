0a737ea22fcecb35d998dcf3f51c44177c3622f6
OkolieOKS234 <dokolie040@gmail.com>
2023-02-21 13:29:12 +0100

    Added a frontend to my little Dapp

--- src/myBank_assets/src/index.js
+++ src/myBank_assets/src/index.js
@@ -1,19 +1,6 @@
-import { myBank } from "../../declarations/myBank";
+import {myBank} from "../../declarations/myBank";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
-
-  const name = document.getElementById("name").value.toString();
-
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await myBank.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
-});
+window.addEventListener("load", async function(){
+    const currentAmount = await  myBank.checkBalance();
+    document.getElementById("value").innerText = currentAmount;
+})
